54ec4bc68398ff3c28a556012ca235244e4698ce
FJCU Intelligent System Laboratory <88232476+fjcu-islab@users.noreply.github.com>
2023-08-24 21:12:16 +0800

    Add files via upload

---
+++ heatmap/vitmain.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from utils2 import GradCAM, show_cam_on_image, center_crop_img
+from vit_model import vit_base_patch16_224_in21k as create_model
+
+
+class ReshapeTransform:
+    def __init__(self, model):
+        input_size = model.patch_embed.img_size
+        patch_size = model.patch_embed.patch_size
+        self.h = input_size[0] // patch_size[0]
+        self.w = input_size[1] // patch_size[1]
+
+    def __call__(self, x):
+        # remove cls token and reshape
+        # [batch_size, num_tokens, token_dim]
+        result = x[:, 1:, :].reshape(x.size(0),
+                                     self.h,
+                                     self.w,
+                                     x.size(2))
+
+        # Bring the channels to the first dimension,
+        # like in CNNs.
+        # [batch_size, H, W, C] -> [batch, C, H, W]
+        result = result.permute(0, 3, 1, 2)
+        return result
+
+
+def main():
+    model = create_model(num_classes=3, has_logits=False)
+    weights_path = "./model-5.pth"
+    model.load_state_dict(torch.load(weights_path, map_location="cpu"))
+    # Since the final classification is done on the class token computed in the last attention block,
+    # the output will not be affected by the 14x14 channels in the last layer.
+    # The gradient of the output with respect to them, will be 0!
+    # We should chose any layer before the final attention block.
+    target_layers = [model.blocks[-1].norm1]
+
+    data_transform = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+    # load image
+    img_path0 = "mildmoderate.jpg"
+    img_path1 = "normal.jpg"
+    img_path2 = "severe.jpg"
+    #assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    img = Image.open(img_path0).convert('RGB')
+    img = np.array(img, dtype=np.uint8)
+    img = center_crop_img(img, 224)
+    # [C, H, W]
+    img_tensor = data_transform(img)
+    # expand batch dimension
+    # [C, H, W] -> [N, C, H, W]
+    input_tensor = torch.unsqueeze(img_tensor, dim=0)
+
+    cam = GradCAM(model=model,
+                  target_layers=target_layers,
+                  use_cuda=False,
+                  reshape_transform=ReshapeTransform(model))
+    target_category = 0  # 0:mildmoderate 1:normal 2:severe
+
+    grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
+
+    grayscale_cam = grayscale_cam[0, :]
+    visualization = show_cam_on_image(img / 255., grayscale_cam, use_rgb=True)
+    plt.imshow(visualization)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
