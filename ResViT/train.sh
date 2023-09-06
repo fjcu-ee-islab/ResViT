3bd58acc7f910acfa2f0565a6a47f7049dd7d4c8
FJCU Intelligent System Laboratory <88232476+fjcu-islab@users.noreply.github.com>
2023-09-07 04:02:41 +0800

    Update train.sh

--- ResViT/train.sh
+++ ResViT/train.sh
@@ -1,1 +1,4 @@
+python convout64.py --classname="normal" --path="train/normal/"
+python convout64.py --classname="severe" --path="train/severe/"
+
 python train.py --num_classes=2 --epochs=50 --batch-size=128 --pick=123 --data-path="train"
